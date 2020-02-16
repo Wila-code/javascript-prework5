86673c967f663d8a218daa706502dd4eb2eb803d
Wilka Gmurczyk <xwilmax@o2.pl>
2020-02-16 21:06:03 +0100

    Add playerInput

--- js/script.js
+++ js/script.js
@@ -39,6 +39,8 @@
   let computerMove = getMoveName(randomNumber);
   printMessage('Mój ruch to: ' + computerMove);
 
+  let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
   console.log('Gracz wpisał: ' + playerInput);
   let playerMove = getMoveName(playerInput);
 
