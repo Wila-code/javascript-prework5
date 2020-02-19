6c9640e42108c57e660bbf9b8b6edac894d7a9aa
Wilka Gmurczyk <xwilmax@o2.pl>
2020-02-19 11:32:12 +0100

    Add const to playerMove and computerMove

--- js/script.js
+++ js/script.js
@@ -37,13 +37,13 @@ const playGame = function (playerInput) {
   let randomNumber = Math.floor(Math.random() * 3 + 1);
   console.log('Wylosowana liczba to: ' + randomNumber);
 
-  let computerMove = getMoveName(randomNumber);
+  const computerMove = getMoveName(randomNumber);
   printMessage('Mój ruch to: ' + computerMove);
 
   /*let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');*/
 
   console.log('Gracz wpisał: ' + playerInput);
-  let playerMove = getMoveName(playerInput);
+  const playerMove = getMoveName(playerInput);
 
   printMessage(displayResult(computerMove, playerMove));
 
